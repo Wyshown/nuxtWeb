79b1fc88698d9f07ef20ea64af358d2cd5cfb5d9
Wyshown <wyshown@gmail.com>
2019-01-08 10:12:24 +0800

    Update env.js
    修改图片的基本配置

--- store/env.js
+++ store/env.js
@@ -1,4 +1,5 @@
-const imgRootUrl = 'https://www.gaoxun.com/savePath/'
+// const imgRootUrl = 'https://www.gaoxun.com/savePath/'
+const imgRootUrl = 'http://101.200.83.32:8103/savePath/'
 
 const ENV = {
   state: () => {
